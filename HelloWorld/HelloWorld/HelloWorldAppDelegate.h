8f4a55184446adebb128281891610d20715bb1e8
Seong Jae Lee <seongjae@gmail.com>
2011-07-30 19:24:09 +0900

    HelloWorld project is added.

---
+++ HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by 최 재규 on 7/30/11.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ HelloWorld/HelloWorldTests/HelloWorldTests.m
@@ -0,0 +1,32 @@
+//
+//  HelloWorldTests.m
+//  HelloWorldTests
+//
+//  Created by 최 재규 on 7/30/11.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import "HelloWorldTests.h"
+
+@implementation HelloWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloWorldTests");
+}
+
+@end
